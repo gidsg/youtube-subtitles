d69762607a08c4a52f97e327046f3f921456f5b5
Gideon Goldberg <gidsgoldberg@gmail.com>
2017-07-10 17:24:43 +0100

    remove unused stuff

--- src/main/scala/youtube/Main.scala
+++ src/main/scala/youtube/Main.scala
@@ -1,14 +1,5 @@
 package youtube
 
-import java.io.File
-
-import com.typesafe.config.{Config, ConfigFactory}
-
-import scala.io.Source
-
-/**
-  * Created by ggoldberg on 10/07/2017.
-  */
 object Main extends App with YouTubeAccess  {
 println("Hello, World!")
 
