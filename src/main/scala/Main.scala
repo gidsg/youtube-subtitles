32b92a98cb5529045fd39888a70c2852ff233e2b
Gideon Goldberg <gidsgoldberg@gmail.com>
2017-07-10 15:35:04 +0100

    add youtube project skeleton

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,5 @@
+package youtube
+
+object Main extends App {
+  println("Hello, World!")
+}
---
+++ src/main/scala/Settings.scala
@@ -0,0 +1,24 @@
+package youtube
+
+import com.typesafe.config.Config
+import scala.collection.JavaConverters._
+
+trait Settings {
+  def config: Config
+
+  def getString(name: String): Option[String] = if(config.hasPath(name)) { Some(config.getString(name)) } else { None }
+  def getStringList(name: String): List[String] = if(config.hasPath(name)) { config.getStringList(name).asScala.toList } else { List.empty}
+  def getBoolean(name: String): Option[Boolean] = if(config.hasPath(name)) { Some(config.getBoolean(name)) } else { None }
+
+  def getMandatoryString(name: String, hint: String = ""): String = if(config.hasPath(name)) {
+    config.getString(name)
+  } else {
+    throw new IllegalArgumentException(s"Missing $name $hint")
+  }
+}
+
+object Settings {
+  def apply(raw: Config): Settings = new Settings {
+    override def config: Config = raw
+  }
+}
