370643a5e273174a5ae4e7108ad46ff02f2335a0
Gideon Goldberg <gidsgoldberg@gmail.com>
2017-07-11 10:48:53 +0100

    query existing caption track

--- src/main/scala/youtube/Main.scala
+++ src/main/scala/youtube/Main.scala
@@ -4,5 +4,7 @@ object Main extends App with YouTubeAccess  {
 println("Hello, World!")
 
   val captions = client.captions()
-  println(captions)
+  val req = captions.list("snippet", "lUpCqBonOFw")
+  val items = req.execute().getItems
+  println(items)
 }
