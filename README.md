636be1f90c9d8a0babef36e559a17908101134be
Gids <gidsgoldberg@gmail.com>
2017-07-21 11:56:29 -0400

    add readme, reference conf and download method example

---
+++ README.md
@@ -0,0 +1,7 @@
+# YouTube subtitles downloader
+API client example which downloads subtitles for a video on your channel
+
+# Running
+* Get YouTube API
+* Copy `reference.conf` to `~/.youtube-subtitles/youtube-subtitles.conf` and populate fields
+* `sbt run`
--- src/main/scala/youtube/Main.scala
+++ src/main/scala/youtube/Main.scala
@@ -1,10 +1,17 @@
 package youtube
 
+import java.io.FileOutputStream
+
 object Main extends App with YouTubeAccess  {
 println("Hello, World!")
 
   val captions = client.captions()
-  val req = captions.list("snippet", "lUpCqBonOFw")
-  val items = req.execute().getItems
+  val listReq = captions.list("snippet", "lUpCqBonOFw")
+  val items = listReq.execute().getItems
   println(items)
+
+  val req = captions.download("Y6HXK1vdgdcx49kZ5e227-0X2IZMdDf8t_GbleN3tqA=").setTfmt("SRT")
+  val outputStream  = new FileOutputStream("captionFile.srt")
+  req.executeMediaAndDownloadTo(outputStream)
+
 }
